4801f5be93cd48a30ee1e3dd3b7daea0d4678e0c
Aman <fullstackdev.aman@gmail.com>
2025-07-26 12:49:18 +0530

    nic going, will be finished ny today

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,6 +1,8 @@
 import logo from "../assets/Logo.svg";
 import heroimage from "../assets/group-1-photo.png";
 import "./landing.css";
+import React, { useState } from "react";
+import {Menu, X} from 'lucide-react'
 
 const LandingPage = () => {
   return (
@@ -14,31 +16,41 @@ const LandingPage = () => {
 };
 
 
-const Box1 = () =>{
-  return(
+const Box1 = () => {
+  
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNav = () =>{
+    setIsOpen(!isOpen);
+  }
+
+  return (
     <>
       <div className="box1">
         <div className="navbar">
           <div className="img">
-          <img src={logo} alt="logo" />
-        </div>
-          <div>
-            <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#tutorials">Tutorials</a></li>
-            <li><a href="#projects">Find Projects</a></li>
+            <img src={logo} alt="logo" />
+          </div>
+          <div >
+            <ul className="navlinks">
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li><a href="#tutorials">Tutorials</a></li>
+              <li><a href="#projects">Find Projects</a></li>
+              <li className="nav-gap"/>
+              <li><a href="#account" className="black">Account</a></li>
+              <li><a href="#dashboard" className="black">Dashboard</a></li>
             </ul>
+            
           </div>
         </div>
 
         <div className="textcontainer">
           <div className="herotext">
-          <h1>Build Together, Smarter</h1>
-          <h2>Find Projects   | Join Teams</h2>
-          <h2>Create projects | Make teams</h2>
-          <h3>An AI-powered platform to join or create group projects, collaborate, and grow no experience needed.</h3>
+            <h1>Build Together, Smarter</h1>
+            <h2>Find Projects     | Join Teams</h2>
+            <h2>Create projects | Make Teams</h2>
+            <h3>An AI-powered platform to join or create group projects, collaborate, and grow no experience needed.</h3>
           </div>
         </div>
       </div>
@@ -46,17 +58,16 @@ const Box1 = () =>{
   );
 }
 
-const Box2 = () =>{
-  return(
+const Box2 = () => {
+  return (
     <>
       <div className="box box2">
         <ul>
-            <li><a href="#account">Account</a></li>
-            <li><a href="#dashboard">Dashboard</a></li>
+          
         </ul>
 
         <div className="img">
-            <img src={heroimage} alt="heroimage" />
+          <img src={heroimage} alt="heroimage" />
         </div>
 
         <div className="bigimage">
