1e91acc749c90c9a5a264623852fb7055a081ebd
Aman <fullstackdev.aman@gmail.com>
2025-07-29 12:01:56 +0530

    finally completed navbar

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -2,49 +2,63 @@ import logo from "../assets/Logo.svg";
 import heroimage from "../assets/group-1-photo.png";
 import "./landing.css";
 import React, { useState } from "react";
-import {Menu, X} from 'lucide-react'
+import { Menu, X } from 'lucide-react'
 
 const LandingPage = () => {
   return (
     <>
       <div className="mainPage">
-        <Box1></Box1>
-        <Box2></Box2>
+        <div className="nav-container">
+          <Navbar></Navbar>
+        </div>
+
+        <div className="box1-container">
+          {/* <Box1></Box1> */}
+        </div>
+
+        <div className="box2-container">
+          {/* <Box2></Box2> */}
+        </div>
       </div>
     </>
   );
 };
 
 
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div className="img">
+          <img src={logo} alt="logo" />
+        </div>
+        <div >
+          <ul className="nav-left">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#tutorials">Tutorials</a></li>
+            <li><a href="#tutorials">LeaderBoard</a></li>
+            <li><a href="#tutorials">Contribute</a></li>
+            <li><a href="#projects">Find Projects</a></li>
+          </ul>
+
+          <ul className="nav-right">
+            <li><a href="#account" className="black">Account</a></li>
+            <li><a href="#dashboard" className="black">Dashboard</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
 const Box1 = () => {
-  
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleNav = () =>{
-    setIsOpen(!isOpen);
-  }
 
   return (
     <>
       <div className="box1">
-        <div className="navbar">
-          <div className="img">
-            <img src={logo} alt="logo" />
-          </div>
-          <div >
-            <ul className="navlinks">
-              <li><a href="#home">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
-              <li><a href="#tutorials">Tutorials</a></li>
-              <li><a href="#projects">Find Projects</a></li>
-              <li className="nav-gap"/>
-              <li><a href="#account" className="black">Account</a></li>
-              <li><a href="#dashboard" className="black">Dashboard</a></li>
-            </ul>
-            
-          </div>
-        </div>
-
         <div className="textcontainer">
           <div className="herotext">
             <h1>Build Together, Smarter</h1>
@@ -60,23 +74,19 @@ const Box1 = () => {
 
 const Box2 = () => {
   return (
-    <>
-      <div className="box box2">
-        <ul>
-          
-        </ul>
+    <div className="box box2">
+      <ul>{/* optional nav links */}</ul>
 
-        <div className="img">
-          <img src={heroimage} alt="heroimage" />
-        </div>
-
-        <div className="bigimage">
-          <img src={logo} alt="big logo" />
-        </div>
+      <div className="img">
+        <img src={heroimage} alt="heroimage" />
       </div>
-    </>
+
+      <div className="bigimage">
+        <img src={logo} alt="big logo" />
+      </div>
+    </div>
   );
-}
+};
 
 
 
